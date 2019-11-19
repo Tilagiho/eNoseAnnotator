54c239a7e030ac22238375e094292586e8b4e458
Timo Land <timo.land@gmx.de>
2019-11-19 12:07:35 +0100

    Extended mvector.h
    MVector class holds String of user defined and detected class name and its abreviation respectively.
    
    Signed-off-by: Timo Land <timo.land@gmx.de>

--- app/mvector.h
+++ app/mvector.h
@@ -20,7 +20,33 @@ public:
 
     bool operator !=(const MVector &other) const;
 
+    /*
+     * contains the sensor values measured
+     */
     std::array<double, size> array;
+
+    /*
+     * class annotated by the user
+     * -> can be used as base truth
+     */
+    QString userDefinedClass = "";
+
+    /*
+     * abreviation of the user defined class name
+     * used in lineGraph
+     */
+    QString userDefinedClassBrief = "";
+
+    /*
+     * automatically detected class
+     */
+    QString detectedClass = "";
+
+    /*
+     * abreviation of the automatically detected class
+     * should be as short as possible
+     */
+    QString detectedClassBrief = "";
 
     /*
      * returns MVector with all elements being zero initialzed
