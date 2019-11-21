d5f2fb98cfd27cc79f90fe5fdfcf178ac4c2025d
Timo Land <timo.land@gmx.de>
2019-11-21 20:30:03 +0100

    Extended aClass

--- app/aclass.cpp
+++ app/aclass.cpp
@@ -1,6 +1,54 @@
 #include "aclass.h"
 
 aClass::aClass(QString className, QString classAbreviation):
-    className{className},
-    classAbreviation{classAbreviation}
+    name{className},
+    abreviation{classAbreviation}
 {}
+
+void aClass::setClass(QString className, QString classAbreviation)
+{
+    this->name = className;
+    this->abreviation = classAbreviation;
+}
+
+bool aClass::isEmpty()
+{
+    return (name == "") || (abreviation == "");
+}
+
+QString aClass::getName() const
+{
+    return name;
+}
+
+QString aClass::getAbreviation() const
+{
+    return abreviation;
+}
+
+QString aClass::toString()
+{
+    if (isEmpty())
+        return "";
+    else
+        return getName() + "[" + getAbreviation() + "]";
+}
+
+aClass aClass::fromString(QString string)
+{
+    auto list = string.split("[");
+    QString name = list[0];
+    QString abreviation = list[1].split("]")[0];
+
+    return aClass(name, abreviation);
+}
+
+bool aClass::operator ==(const aClass &b) const
+{
+    return (name==b.getName()) && (abreviation==b.getAbreviation());
+}
+
+bool aClass::operator !=(const aClass &b) const
+{
+    return (name!=b.getName()) && (abreviation!=b.getAbreviation());
+}
--- app/aclass.h
+++ app/aclass.h
@@ -1,7 +1,7 @@
 #ifndef ACLASS_H
 #define ACLASS_H
 
-#include <QString>
+#include <QtCore>
 
 /*
  * Class of vector
@@ -9,10 +9,28 @@
 class aClass
 {
 public:
-    aClass(QString className, QString classAbreviation);
+    aClass(QString name, QString abreviation);
 
-    QString className;
-    QString classAbreviation;
+    bool isEmpty();
+
+    void setClass(QString name, QString abreviation);
+
+    QString getName() const;
+
+    QString getAbreviation() const;
+
+    QString toString();
+
+    static aClass fromString(QString string);
+
+    bool operator ==(const aClass &b) const;
+
+    bool operator !=(const aClass &b) const;
+
+private:
+
+    QString name;
+    QString abreviation;
 };
 
 #endif // ACLASS_H
